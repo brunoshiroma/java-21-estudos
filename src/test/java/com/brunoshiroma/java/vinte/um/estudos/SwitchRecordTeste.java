918596640a56c752a972fc0410cd8d53f6c55ba9
Bruno Shiroma <brunoshiroma@gmail.com>
2024-02-21 23:54:48 +0000

    chore: config auto formatter

--- src/test/java/com/brunoshiroma/java/vinte/um/estudos/SwitchRecordTeste.java
+++ src/test/java/com/brunoshiroma/java/vinte/um/estudos/SwitchRecordTeste.java
@@ -6,37 +6,33 @@ import org.junit.jupiter.api.Test;
 
 /**
  * Estudos dos novos usos do switch
- * 
- * @see https://docs.oracle.com/en/java/javase/21/language/pattern-matching-switch-expressions-and-statements.html
+ *
+ * @see
+ *     https://docs.oracle.com/en/java/javase/21/language/pattern-matching-switch-expressions-and-statements.html
  * @see https://spring.io/blog/2023/09/20/hello-java-21#enhanced-switch
  */
-
 class SwitchRecordTeste {
 
-    record Usuario(String nome) {
-    }
+  record Usuario(String nome) {}
 
-    record Produto(String nome, double preco) {
-    }
+  record Produto(String nome, double preco) {}
 
-    @Test
-    void testeSwitchRecordMatching() {
+  @Test
+  void testeSwitchRecordMatching() {
 
-        final var usuario1 = new Usuario("1");
-        final var produto1 = new Produto("Produto 1", 1);
+    final var usuario1 = new Usuario("1");
+    final var produto1 = new Produto("Produto 1", 1);
 
-        assertEquals("Usuário 1", teste(usuario1));
-        assertEquals("Produto Produto 1, custa 1.0", teste(produto1));
+    assertEquals("Usuário 1", teste(usuario1));
+    assertEquals("Produto Produto 1, custa 1.0", teste(produto1));
+  }
 
-    }
+  static String teste(Object objeto) {
 
-    static String teste(Object objeto) {
-
-        return switch (objeto) {
-            case Usuario(var nome) -> STR."Usuário \{nome}";
-            case Produto(var nome, var preco) -> STR."Produto \{nome}, custa \{preco}";
-            default -> null;
-        };
-    }
-
+    return switch (objeto) {
+      case Usuario(var nome) -> STR."Usuário \{nome}";
+      case Produto(var nome, var preco) -> STR."Produto \{nome}, custa \{preco}";
+      default -> null;
+    };
+  }
 }
